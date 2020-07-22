1a73b925b556cb60acf28178206c253295785a49
bruj0 <ramakandra@gmail.com>
2020-07-22 10:17:57 +0200

    added module source

--- clusterA/main.tf
+++ clusterA/main.tf
@@ -59,10 +59,11 @@ resource "azuread_service_principal_password" "aks_service_principle" {
 # =============================================
 # main
 # =============================================
+# Using rhythmicthech repo because the official Azure implementation doesnt support the `network_profile` block.
 
 module "aks" {
-  source = "../"
-  #   source = "github.com/Azure/terraform-azurerm-aks?ref=v2.0"
+  #source = "../"
+  source = "github.com/rhythmictech/terraform-azurerm-aks?ref=v5.1.0"
 
   prefix             = var.name
   kubernetes_version = local.aks_version
@@ -76,22 +77,22 @@ module "aks" {
     docker_bridge_cidr = null
     pod_cidr           = null
     service_cidr       = null
-    load_balancer_sku = "standard"
+    load_balancer_sku  = "standard"
   }
-  default_node_pool =  {
-    name                = "nodepool"
-    vm_size             = "standard_f2"
-    enable_auto_scaling = true
-    os_disk_size_gb     = 50
-    type                = "VirtualMachineScaleSets"
-    max_count           = 3
-    min_count           = 1
+  default_node_pool = {
+    name                  = "nodepool"
+    vm_size               = "standard_f2"
+    enable_auto_scaling   = true
+    os_disk_size_gb       = 50
+    type                  = "VirtualMachineScaleSets"
+    max_count             = 3
+    min_count             = 1
     enable_node_public_ip = false
   }
-    tags = {
-      environment = "Demo"
-      Owner = "rodrigo@hashicorp.com"
-    }     
+  tags = {
+    environment = "Demo"
+    Owner       = "rodrigo@hashicorp.com"
+  }
 }
 
 # =============================================
